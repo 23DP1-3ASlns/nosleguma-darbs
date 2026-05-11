dd3ad16f31fd8da252b131983dc6e9bee27528d5
Artis Silins <liltacc7@gmail.com>
2026-05-11 22:51:30 +0300

    run migrations on start postgresql

--- school-backend/start.sh
+++ school-backend/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-set -e
+echo "Clearing config..."
+php artisan config:clear
 echo "Running migrations..."
 php artisan migrate --force --seed
-echo "Migrations done!"
 echo "Starting server..."
 php artisan serve --host=0.0.0.0 --port=8080
