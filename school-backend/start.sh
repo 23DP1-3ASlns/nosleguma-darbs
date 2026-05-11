e76a9a87749f042347d34aab9ad4a65c45b5d560
Artis Silins <liltacc7@gmail.com>
2026-05-11 22:08:15 +0300

    fix start script

--- school-backend/start.sh
+++ school-backend/start.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-php artisan config:clear
+set -e
+echo "Running migrations..."
 php artisan migrate --force --seed
+echo "Migrations done!"
+echo "Starting server..."
 php artisan serve --host=0.0.0.0 --port=8080
